57ce7a15e8bae76c4cef6f99be267a412dc9fb84
MadisonViaLux <madison.s.mckown@gmail.com>
2019-10-15 15:02:28 -0700

    formatting to use cookies and adding restricted routes with a logout

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
 const express = require('express')
 const Users = require('./users/user-router')
+const sessions = require('express-session')
+const knexSession = require('connect-session-knex')(sessions)
+const dbConfig = require('./data/db-config')
 const server = express()
 
+
+const sessionConfig = {
+    name: 'Madison-M',
+    secret: 'my ssn is 111*11*1111',
+    cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60,
+        secure: false
+    },
+    resave: false,
+    saveUnilitialized: true,
+    store: new knexSession({
+        knex: dbConfig,
+        createtable: true,
+        clearInterval: 1000 * 60 * 30
+    })
+}
+
+
+
+server.use(sessions(sessionConfig))
 server.use(express.json())
 server.use('/api', Users)
+
+
+
+
+server.get('/', (req, res) => {
+    res.status(200).json(console.log('WORKING'))
+});
+
 
 
 module.exports = server
